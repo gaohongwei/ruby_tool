aab506936962685348ad965002b2cd43f1486af4
gary <gaohongwei@gmail.com>
2014-03-01 13:45:48 -0800

    Add regular expression samples

---
+++ rexp.rb
@@ -0,0 +1,7 @@
+s="link_to 'New Part', new_part_path"
+s.sub(/link_to (.*),(.*)/,'\1\2')
+s.sub(/link_to (.*),(.*)/,'t(\1),\2')
+s.sub(/link_to (.*),(.*)/,'link_to t(\1),\2')
+
+ps={pold:"link_to (.*),(.*)",pnew:"link_to t(\\1),\\2"}
+s.sub(/#{ps[:pold]}/, "#{ps[:pnew]}")
