87bcb276acbc1dd473ddcbb743578dc159c61625
gary <gaohongwei@gmail.com>
2014-03-20 14:14:33 -0700

    Create install_iso.sh

---
+++ install_iso.sh
@@ -0,0 +1,7 @@
+sudo passwd root
+apt-get install openssh-server
+sudo apt-get install curl
+\curl -L https://get.rvm.io | bash -s stable
+exit and login
+rvm install ruby -v 2.1.1
+gem install rails
