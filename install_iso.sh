13d20fc829741a3dbb8f5c0b60a23c997f00e8be
gary <gaohongwei@gmail.com>
2014-03-25 14:47:14 -0700

    Update install_iso.sh

--- install_iso.sh
+++ install_iso.sh
@@ -9,3 +9,17 @@ gem install heroku
 rvm install ruby -v 2.1.1
 gem install --no-rdoc --no-ri rails -v 4.0.3
 apt-get install -y postgresql postgresql-contrib libpq-dev
+
+postgres
+sudo su postgres -c  "createuser root --pwprompt"  
+sudo -u postgres dropdb   $dbname
+sudo -u postgres createdb $dbname
+
+ /etc/init.d/postgresql status
+ 
+ psql -l
+
+ psql TCM_production
+SELECT version();
+\dt
+\q
