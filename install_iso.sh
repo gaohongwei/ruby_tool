2b113b978616c686d1c0a11270a2779e2a6161c4
gary <gaohongwei@gmail.com>
2014-03-25 12:14:00 -0700

    Update install_iso.sh

--- install_iso.sh
+++ install_iso.sh
@@ -1,7 +1,8 @@
 sudo passwd root
 apt-get install openssh-server
-sudo apt-get install curl
+sudo apt-get -y install curl
 \curl -L https://get.rvm.io | bash -s stable
 exit and login
 rvm install ruby -v 2.1.1
-gem install rails
+gem install --no-rdoc --no-ri rails -v 4.0.3
+apt-get install -y postgresql postgresql-contrib libpq-dev
