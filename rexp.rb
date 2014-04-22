6d8826a72657b920a0c13d4938522e063d638eec
Kevin <gaohongwei@gmail.com>
2014-04-22 15:33:12 -0700

    Update rexp.rb

--- rexp.rb
+++ rexp.rb
@@ -9,3 +9,24 @@ var='abc'
 if s =~/^#{var}/ 
   
 end
+
+###  .*,  Agressive
+###  .*?, Non-Agressive
+
+#######################################
+## Add id according to class name
+#file_names = ['foo.txt', 'bar.txt']
+file_names=Dir.glob("template/*.html")
+file_names.each do |file_name|
+  text = File.read(file_name)
+  #puts text.gsub(/search_regexp/, "replace string")
+  #<div class="xtab scan_tab tab_btn" onclick="doAction('scan', null);">
+  t=text.gsub(/<div class=\"xtab (.*?) (.*?)\"/,'<div id="\1" class="xtab \1 \2"')
+  #puts "id=" + $1 unless $1.nil?
+  puts t
+  if text != t 
+    puts file_name
+  File.open(file_name, "w") {|file| file.puts t}
+  end
+end
+##################
