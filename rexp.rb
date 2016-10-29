586def971a7910bc1f4c8503095e457d05b27bcb
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-10-28 22:05:51 -0700

    Update rexp.rb

--- rexp.rb
+++ rexp.rb
@@ -59,3 +59,42 @@ module KeywordValidator
       /x).map(&:strip)
   end
 end
+##################
+Regular Expression Basic
+Anchor
+  ^: line header
+  $: line tailer
+  \A: string header
+  \z: string tailer (no new line)
+  \Z: string tailer
+
+Pattern:
+  /./ - Any character except a newline.
+  /./m - Any character (the m modifier enables multiline mode)
+  /\d/: A digit character ([0-9])
+  /\D/: 
+  /\w/: A word character ([a-zA-Z0-9_])
+  /\W/: non word char, [^a-zA-Z0-9]+
+  /\s/: A whitespace character: /[ \t\r\n\f]/
+  /\S/:
+  /\h/: hex
+  /\H/:
+
+Repetition
+*: >=0
++: >=1
+?: 0/1
+{n}: Exactly n times
+{n,}: n or more times
+{,m}: m or less times
+{n,m}: At least n and at most m times
+
+1. %r{... } : (/... /)
+  expect(rendered).to match %r{<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>}
+
+2. Interpolating a string into a regex
+uid='1234'
+test = ["1234_o365" , "1000"]
+test.find{|x| /#{uid}/ =~ x}
+
+if goo =~ /#{Regexp.quote(uid)}/
